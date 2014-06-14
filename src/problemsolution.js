e4bae635990c8f6be359cac1dfe64108f1675e9c
kmaune0 <kmaune71@gmail.com>
2014-06-13 18:59:55 -0700

    recursion with memorization

---
+++ src/problemsolution.js
@@ -0,0 +1,42 @@
+// problemsolution.js
+// (C) 2014 kmaune0
+
+// A child is running up a staircase with n steps, and can hop either 1 step,
+// 2 steps, or 3 steps at a time. Implement a method to count how many possible
+// ways the child can run up the stairs.
+
+
+module.exports = Staircase = (function() {
+  var memo = [];
+
+  var count = function(n) {
+    return countWays(n, 0);
+  };
+
+  var countWays = function(remaining) {
+    if (remaining < 0) {
+      return 0;
+    }
+
+    if (remaining == 0) {
+      return 1;
+    }
+
+    if (memo[remaining]) {
+      return memo[remaining];
+    }
+
+    else {
+      memo[remaining] = countWays(remaining - 1)
+      + countWays(remaining - 2)
+      + countWays(remaining - 3);
+
+      return memo[remaining];
+    }
+  };
+
+  return {
+    count: count
+  };
+
+}) ();
