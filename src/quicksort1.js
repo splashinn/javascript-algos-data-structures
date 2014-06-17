7a6926c4c6b6385a699cf41770e92653c3346a54
kmaune0 <kmaune71@gmail.com>
2014-06-17 14:35:40 -0700

    added another quicksort implementation

---
+++ src/quicksort1.js
@@ -0,0 +1,30 @@
+// quicksort1.js
+// Quicksort implementation
+// (C) 2014 kmaune0
+
+var quicksort = function(arr) {
+  var result = [];
+  if(arr.length === 2) {
+    result.push(Math.min.apply(null, arr));
+    result.push(Math.max.apply(null, arr));
+  } else if(arr.length === 1) {
+    result.push(arr[0]);
+  } else if(arr.length > 2) {
+    var pivotIndex = Math.floor(arr.length/2);
+    var pivot = arr[pivotIndex];
+    arr.splice(pivotIndex, 1);
+    var halfOfArr1 = [];
+    var halfOrArr2 = [];
+
+    for(var i = 0; i < arr.length; i++) {
+      if(arr[i] <= pivot) {
+        halfOfArr1.push(arr[i]);
+      } else {
+        halfOfArr2.push(arr[i]);
+      }
+    }
+    result = quicksort(halfOfArr1).concat(pivot).concat(quicksort(halfOfArr2));
+  }
+
+  return result;
+};
