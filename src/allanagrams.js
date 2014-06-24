88655028d4bdd44afad898277a9ec66c359dbb54
kmaune0 <kmaune71@gmail.com>
2014-06-23 21:58:08 -0700

    added all anagrams file
    this file finds all the anagrams for the word entered.

---
+++ src/allanagrams.js
@@ -0,0 +1,21 @@
+// allanagrams.js
+// (C) 2014 splashinn
+
+function allanagrams (word) {
+  if (word.length < 2) {
+    return [word];
+  } else {
+      var allanswers = [];
+      for (var i = 0; i < word.length; i++) {
+        var letter = word[i];
+        var shorterword = word.substr(0, i) + word.substr(i + 1, word.length - 1);
+        var shortwordarray = allanagrams(shorterword);
+        for (var j = 0; j < shortwordarray.length; j++) {
+          allanswers.push(letter + shortwordarray[j]);
+        }
+      }
+      return allanswers;
+  }
+}
+
+allanagrams("")
