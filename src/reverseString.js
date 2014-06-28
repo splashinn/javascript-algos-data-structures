416fe252533d32d79c7efa9576d48edab62eb4d4
Kyle Maune <kmaune71@gmail.com>
2014-06-28 11:57:52 -0700

    added function for reversing a string
    Enter a string and it will be reversed and printed out.

---
+++ src/reverseString.js
@@ -0,0 +1,10 @@
+// reverseString.js
+// (C) 2014 splashinn
+// Enter a string and it will be reversed and printed out
+// Add any string to this function and when it is called it will be reversed
+
+function reverseIt(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log(reverseIt(" "));
