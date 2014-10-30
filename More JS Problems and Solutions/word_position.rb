b0f3802092b6ff402779eec697e957c665083646
Kyle Maune <kmaune71@gmail.com>
2014-10-30 14:22:59 -0700

    added word position ruby solution

---
+++ More JS Problems and Solutions/word_position.rb
@@ -0,0 +1,11 @@
+# word_position.rb
+# splashinn
+
+def word_position(text, word)
+  text = text.split(" ")
+  positions = []
+  text.each_index do |i|
+    positions << i if text[i] == word
+  end
+  return positions
+end
